b90ac218934b33cc967cb064b8bcfccb9df09869
sofia.larborn@gmail.com <Morvar>
2017-03-31 19:53:26 +0200

    create in-game screen and main menu screen classes in a screens-folder

---
+++ core/src/com/mygdx/game/screens/GameScreen.java
@@ -0,0 +1,10 @@
+package com.mygdx.game.screens;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+/**
+* Screen that contains the game (in-game).
+*/
+public class GameScreen extends ScreenAdapter{
+
+}
---
+++ core/src/com/mygdx/game/screens/MainMenuScreen.java
@@ -0,0 +1,10 @@
+package com.mygdx.game.screens;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+/**
+* Screen that contains the main menu.
+*/
+public class MainMenuScreen extends ScreenAdapter{
+
+}
