4ba479de3e515f5a00c0af5b28c586f53ed78832
sofia.larborn@gmail.com <Morvar>
2017-04-01 01:06:20 +0200

    add displayable test content to main menu screen

--- core/src/com/mygdx/game/UnexpectedMagic.java
+++ core/src/com/mygdx/game/UnexpectedMagic.java
@@ -1,33 +1,44 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.screens.MainMenuScreen;
 
-public class UnexpectedMagic extends ApplicationAdapter {
-	SpriteBatch batch;
+public class UnexpectedMagic extends Game {
+	public SpriteBatch batch;
+	public BitmapFont font;
 	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		font = new BitmapFont();
 		img = new Texture("badlogic.jpg");
+		this.setScreen(new MainMenuScreen(this));
+		
 	}
 
 	@Override
 	public void render () {
+		super.render();
+		/*
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+		*/
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
+		font.dispose();
 		img.dispose();
 	}
 }
--- core/src/com/mygdx/game/screens/MainMenuScreen.java
+++ core/src/com/mygdx/game/screens/MainMenuScreen.java
@@ -1,16 +1,24 @@
 package com.mygdx.game.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.mygdx.game.UnexpectedMagic;
 
 /**
 * Screen that contains the main menu.
 */
 public class MainMenuScreen extends ScreenAdapter{
-	UnexpectedMagic game;
+	
+	final UnexpectedMagic game;
+	OrthographicCamera guiCam;
+	float[] viewportDim = {480, 720};
 
-	public MainMenuScreen(UnexpectedMagic game){
+	public MainMenuScreen(final UnexpectedMagic game){
 		this.game = game;
+		guiCam = new OrthographicCamera(viewportDim[0], viewportDim[1]);
+		guiCam.setToOrtho(false, viewportDim[0], viewportDim[1]);
 	}
 	
 	public void update(){
@@ -22,7 +30,13 @@ public class MainMenuScreen extends ScreenAdapter{
 	}
 	@Override
 	public void render(float delta){
-		
+		Gdx.gl.glClearColor(0,0,0,1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		guiCam.update();
+		game.batch.setProjectionMatrix(guiCam.combined);
+		game.batch.begin();
+		game.font.draw(game.batch, "MAIN MENU TEST", viewportDim[0]/2, viewportDim[1]/2);
+		game.batch.end();
 	}
 	
 	@Override
