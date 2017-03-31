c8a3fe9da95c87a71f8cf0ac2ba053e9f845ea7e
sofia.larborn@gmail.com <Morvar>
2017-03-31 20:18:34 +0200

    add some code in the main menu screen class

--- core/src/com/mygdx/game/screens/MainMenuScreen.java
+++ core/src/com/mygdx/game/screens/MainMenuScreen.java
@@ -1,10 +1,33 @@
 package com.mygdx.game.screens;
 
 import com.badlogic.gdx.ScreenAdapter;
+import com.mygdx.game.UnexpectedMagic;
 
 /**
 * Screen that contains the main menu.
 */
 public class MainMenuScreen extends ScreenAdapter{
+	UnexpectedMagic game;
 
+	public MainMenuScreen(UnexpectedMagic game){
+		this.game = game;
+	}
+	
+	public void update(){
+		
+	}
+	
+	public void draw(){
+		
+	}
+	@Override
+	public void render(float delta){
+		
+	}
+	
+	@Override
+	public void pause(){
+		
+	}
+	
 }
