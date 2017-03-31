11649f9948fb86af7fc5ff1a8018232069c84b88
sofia.larborn@gmail.com <Morvar>
2017-03-31 16:35:41 +0200

    reorganize some folders and add two components and one songmap

---
+++ core/src/com/mygdx/game/components/PositionComponent.java
@@ -0,0 +1,10 @@
+package com.mygdx.game.components;
+
+import com.badlogic.ashley.core.Component;
+
+/**
+* Component that represents the sprite.
+*/
+public class PositionComponent implements Component{
+
+}
