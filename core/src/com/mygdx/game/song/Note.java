60cabf11768a2ea9abc865507883afe5d08ecc77
car0b1nius <car0b1nius@localhost>
2017-04-02 13:52:24 +0200

    add Note and Voice classes for file reading

---
+++ core/src/com/mygdx/game/song/Note.java
@@ -0,0 +1,44 @@
+package com.mygdx.game.song;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * A class that represents a single note.
+ */
+public class Note {
+	//static
+	private static Map<String, Map<String, Note>> noteMap = new HashMap<>();
+	public static Note getNote(String note) throws IOException {
+		//allocations
+		Note n;
+		Map<String, Note> pitchMap;
+		String[] sa = note.split(":");
+		String pitch = sa[0], value = sa[1];
+		//sanity checking
+		if(sa.length != 2) throw new IOException("Note should have exactly 1 colon");
+		if(!(pitch.equals("-") || pitch.matches("[a-g][b#]?\\d"))) throw new IOException("Invalid note pitch " + pitch);
+		if(!value.matches("\\d+/\\d+")) throw new IOException("Invalid note value " + value);
+		//note retrieval/construction
+		if(noteMap.containsKey(pitch)) {
+			pitchMap = noteMap.get(pitch);
+		} else {
+			pitchMap = new HashMap<>();
+			noteMap.put(pitch, pitchMap);
+		}
+		if(pitchMap.containsKey(value)) {
+			n = pitchMap.get(value);
+		} else {
+			n = new Note(pitch, value);
+			pitchMap.put(value, n);
+		}
+		return n;
+	}
+	//instance
+	final String pitch;
+	final String value;
+	private Note(String pitch, String value) {
+		this.pitch = pitch;
+		this.value = value;
+	}
+}
---
+++ core/src/com/mygdx/game/song/Voice.java
@@ -0,0 +1,25 @@
+package com.mygdx.game.song;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Voice {
+	private final Note[][] bars;
+	public final String instrument;
+	public Voice(String voice, String instrument) throws IOException {
+		this.instrument = instrument;
+		List<Note[]> bList = new LinkedList<>();
+		for(String bar : voice.split("|")) {
+			List<Note> nList = new LinkedList<>();
+			for(String note : bar.split(",")) {
+				nList.add(Note.getNote(note));
+			}
+			bList.add(nList.toArray(new Note[0]));
+		}
+		bars = bList.toArray(new Note[0][0]);
+	}
+	public Note[][] getBars() {
+		return bars.clone();
+	}
+}
