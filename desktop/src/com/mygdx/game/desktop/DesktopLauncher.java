2887fe199e9e7bb9cc7b9ab380a43a9f2e8a8e9d
sofia.larborn@gmail.com <Morvar>
2017-04-03 08:56:04 +0200

    add some config in desktop app main

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.mygdx.game.UnexpectedMagic;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Unexpected Magic";
+		//config.width = 720;
+		//config.height = 480;
 		new LwjglApplication(new UnexpectedMagic(), config);
 	}
 }
