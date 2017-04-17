44718496a8c541570061c0316602f5cf3a96b657
car0b1nius <car0b1nius@localhost>
2017-04-17 17:01:43 +0200

    add some more testing

--- core/src/com/mygdx/game/test/SongTest.java
+++ core/src/com/mygdx/game/test/SongTest.java
@@ -8,17 +8,17 @@ import java.util.Scanner;
 
 import org.junit.Test;
 
+import com.mygdx.game.model.song.Note;
 import com.mygdx.game.model.song.Song;
 import com.mygdx.game.model.song.Voice;
 
 public class SongTest {
-
 	@Test
-	public void test() throws IOException {
-		System.out.println(System.getProperty("user.dir"));
+	public void testHisTheme() throws IOException {
 		String s;
 		{
-			Scanner sc = new Scanner(new File("assets/songmaps/his_theme.uxm")).useDelimiter("\\Z");
+			Scanner sc = new Scanner(new File("assets/songmaps/his_theme.uxm"));
+			sc.useDelimiter("\\Z");
 			s = sc.next();
 			sc.close();
 		}
@@ -28,5 +28,17 @@ public class SongTest {
 		assertEquals(120, song.bpm);
 		Voice[] voices = song.getVoices();
 		assertEquals(4, voices.length);
+		Note[] bar = voices[0].getBars()[0];
+		assertTrue(bar[0] == bar[3]);
+	}
+	@Test
+	public void testNotes() throws IOException {
+		String note = "A1:1";
+		Note n1 = Note.getNote(note);
+		Note n2 = Note.getNote(note);
+		assertTrue(n1 == n2);
+		note = "A2:1";
+		n2 = Note.getNote(note);
+		assertFalse(n1 == n2);
 	}
 }
