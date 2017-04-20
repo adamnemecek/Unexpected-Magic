f4bbd3a9c583af77da7d4420bc1f50dc1dbc75d7
sofia.larborn@gmail.com <Morvar>
2017-04-20 13:18:48 +0200

    create note array in voice where each index represents a tick

--- core/src/com/mygdx/game/model/song/Voice.java
+++ core/src/com/mygdx/game/model/song/Voice.java
@@ -7,32 +7,37 @@ import java.util.Queue;
 
 public class Voice {
 	private final Note[] notes;
+	
 	public final String instrument;
 	public final int length;
 	public Voice(String voice, String instrument) throws IOException {
 		this.instrument = instrument;
-		/*
-		List<Note[]> bList = new LinkedList<>();
-		for(String bar : voice.split("[,|]")) {
-			List<Note> nList = new LinkedList<>();
-			for(String note : bar.split(",")) {
-				nList.add(Note.getNote(note));
-			}
-			bList.add(nList.toArray(new Note[0]));
-		}
-		bars = bList.toArray(new Note[0][0]);
-		*/
+		
 		List<Note> nList = new LinkedList<>();
+		int sum = 0;
 		for(String note : voice.split("[,|]")) {
-			nList.add(Note.getNote(note));
+			Note n = Note.getNote(note);
+			nList.add(n);
+			sum += n.calculateLength();
 		}
-		notes = nList.toArray(new Note[0]);
-		length = calculateLength();
+		notes = new Note[sum];
+		sum = 0;
+		for(Note note : nList) {
+			notes[sum] = note;
+			sum += note.calculateLength();
+		}
+		oMGIHAVECORNINMYCODEEEEE();
+		length = sum;
 	}
 	public Note[] getNotes() {
 		return notes.clone();
 	}
-	
+	private void oMGIHAVECORNINMYCODEEEEE(){
+		for(int i=0;i<notes.length;i++){
+			System.out.print(notes[i]+", ");
+		}
+	}
+	/*
 	private int calculateLength(){
 		int sum = 0;
 		for(Note note : notes){
@@ -40,4 +45,5 @@ public class Voice {
 		}
 		return sum;
 	}
+	*/
 }
