cef96043d18808a1ff20d6dcc67170fb4d3c39b3
Arvid Runvik <ar.netacc@gmail.com>
2017-04-26 16:04:44 +0200

    the very basics

--- core/src/com/mygdx/game/model/Score.java
+++ core/src/com/mygdx/game/model/Score.java
@@ -4,9 +4,19 @@ public class Score {
 	
 	/**
 	 * A class that holds score.
+	 * Modified by Arunvik
 	 */
+	
+	private int score;
+	private int streak;
+	
 
 	public Score(){
-		
+		score = 0;
+		streak = 0;	
 	}
+	
+	//hej
+	
+	
 }
