cac33a7576ef6f6664c744d9c9be07ea8963faef
car0b1nius <car0b1nius@localhost>
2017-04-28 18:56:04 +0200

    change internal note functionality and instantiation

--- core/src/com/mygdx/game/model/song/Note.java
+++ core/src/com/mygdx/game/model/song/Note.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 /**
  * A class that represents a single note.
+ * @author car0b1nius
  */
 public class Note {
 	//static
@@ -12,43 +13,71 @@ public class Note {
 	public static Note getNote(String note) throws IOException {
 		//sanity checking
 		if(!note.contains("/")) return getNote(note + "/1");
+		/*
+		 * Regex explanation
+		 * [ABC]: any of A, B and C
+		 * [A-C]: any character between A-C (inclusive)
+		 * A?: zero or one A
+		 * \d: any number (needs Java string escaping)
+		 * (ABC): the sequence ABC as a capture group, as a single regex entity
+		 * (ABC|DEF): either sequence ABC or DEF
+		 * A+: any non-zero number of A
+		 * [A-G][b#]?\d: a character A-G, followed by optional b or #, followed by any number
+		 * ([A-G][b#]?\d|-): either the above or a -
+		 * \d+/\d+: Any fraction
+		 * ([A-G][b#]?\\d|-):\\d+/\\d+: both the above two lines, separated by a :
+		 */
 		if(!note.matches("([A-G][b#]?\\d|-):\\d+/\\d+")) //Regex might not be the most readable solution, but it's very writable.
 			throw new IOException("Invalid note " + note);
 		if(noteMap.containsKey(note)) return noteMap.get(note);
 		//allocations
 		String[] sa = note.split(":");
-		String pitch = sa[0];
-		sa = sa[1].split("/");
-		int[] value = new int[]{Integer.parseInt(sa[0]), Integer.parseInt(sa[1])};
+		int num = getNum(sa[0]);
+		int dur = getDur(sa[1]);
 		//note construction/storage
-		Note n = new Note(pitch, value);
+		Note n = new Note(note, num, dur);
 		noteMap.put(note, n);
 		return n;
 	}
-	//instance
-	final String pitch;
-	private final int[] value;
-	private Note(String pitch, int[] value) {
-		this.pitch = pitch;
-		this.value = value;
+	private static final int[] MAJOR_SCALE = new int[] {0,2,4,5,6,9,11};
+	private static int getNum(String str) {
+		char[] chs = str.toCharArray();
+		char pitch = chs[0];
+		int alt = 0;
+		if(chs.length == 3)
+			switch(chs[1]) {
+			case 'b':
+				alt = -1;
+				break;
+			case '#':
+				alt = +1;
+				break;
+			}
+		int oct = Character.getNumericValue(chs[chs.length-1]);
+		return MAJOR_SCALE["CDEFGAB".indexOf(pitch)] + alt + oct * 12;
 	}
-	public String getPitch() {
-		return pitch;
-	}
-	public int[] getValue() {
-		return value.clone(); //defensive copying
-	}
-	@Override
-	public String toString() {
-		return pitch + ":" + value[0] + "/" + value[1];
-	}
-	
-	public int calculateLength(){
-		int num = value[0];
-		int denom = value[1];
+	private static int getDur(String str) {
+		String[] sa = str.split("/");
+		int num = Integer.valueOf(sa[0]);
+		int denom = Integer.valueOf(sa[1]);
 		int factor = 64 / denom;
 		return num * factor;
 	}
+	//instance
+	public final int number, duration;
+	private final String str;
+	private Note(String str, int number, int duration) {
+		this.number = number;
+		this.duration = duration;
+		this.str = str;
+	}
+	@Override
+	public String toString() {
+		return str;
+	}
 	
-	
+	@Deprecated
+	public int calculateLength(){
+		return duration;
+	}
 }
