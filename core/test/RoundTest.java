a2767103fcd906a356707a1ea19497728fe73c2d
sofia.larborn@gmail.com <Morvar>
2017-05-02 17:38:01 +0200

    add a round test class without tests

---
+++ core/test/RoundTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.mygdx.game.model.Player;
+import com.mygdx.game.model.Round;
+import com.mygdx.game.model.song.Song;
+
+public class RoundTest {
+
+	private Round round;
+	@Before
+	public void init() throws IOException{
+		Song song = new Song("songmaps/his_theme.uxm");
+		ArrayList<Player> players = new ArrayList<>();
+		round = new Round(song, players);
+	}
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
