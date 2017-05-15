a6af72906896d01617c0f41faee0762008872428
Arvid Runvik <ar.netacc@gmail.com>
2017-05-15 15:32:01 +0200

    added (empty) hitmanager class and made SoundSystem not miss notes

---
+++ core/src/com/mygdx/game/gameEngine/managers/HitManager.java
@@ -0,0 +1,5 @@
+package com.mygdx.game.gameEngine.managers;
+
+public class HitManager {
+
+}
--- core/src/com/mygdx/game/gameEngine/systems/SoundSystem.java
+++ core/src/com/mygdx/game/gameEngine/systems/SoundSystem.java
@@ -1,12 +1,16 @@
 package com.mygdx.game.gameEngine.systems;
 
+import java.util.List;
+
 import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
+import com.mygdx.game.gameEngine.components.HitComponent;
 import com.mygdx.game.gameEngine.components.NoteComponent;
 import com.mygdx.game.gameEngine.components.PositionComponent;
 import com.mygdx.game.gameEngine.managers.SoundManager;
+import com.mygdx.game.model.song.IVoice;
 import com.mygdx.game.utilities.file.Constants;
 
 /**
@@ -16,10 +20,12 @@ public class SoundSystem extends IteratingSystem {
 
     private ComponentMapper<PositionComponent> pm = ComponentMapper.getFor(PositionComponent.class);
     private ComponentMapper<NoteComponent> nm = ComponentMapper.getFor(NoteComponent.class);
+    private ComponentMapper<HitComponent> hm = ComponentMapper.getFor(HitComponent.class);
     private SoundManager soundManager;
+    private List<IVoice> autoVoices;
 
     public SoundSystem(SoundManager s) {
-        super(Family.all(PositionComponent.class, NoteComponent.class).get());
+        super(Family.all(PositionComponent.class, NoteComponent.class).get()); //grekiska
         this.soundManager = s;
     }
 
@@ -28,9 +34,11 @@ public class SoundSystem extends IteratingSystem {
     protected void processEntity(Entity entity, float deltaTime) {
         PositionComponent pos = pm.get(entity);
         NoteComponent not = nm.get(entity);
+        HitComponent hit = hm.get(entity);
 
-        if ((int) pos.getY() == Constants.SCORE_BOUNDS_UPPER) { //TODO Is this accurate
+        if ((int) pos.getY() <= Constants.SCORE_BOUNDS_UPPER && !hit.isHit()) { //TODO Is this accurate
             soundManager.play(not.getNote());
+            hit.hit();
             //}
         }
     }
