b730ec64238b47b443372016b9455f81b8bd892e
sofia.larborn@gmail.com <Morvar>
2017-05-16 17:00:21 +0200

    add a main menu button in score screen that sends the user back to a new main menu

--- core/src/com/mygdx/game/gameEngine/screens/ScoreScreen.java
+++ core/src/com/mygdx/game/gameEngine/screens/ScoreScreen.java
@@ -2,8 +2,11 @@ package com.mygdx.game.gameEngine.screens;
 
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 /**
  * Screen that shows score after a game.
@@ -23,9 +26,22 @@ public class ScoreScreen extends AbstractScreen{
 		//text
 		Label testLabel = new Label("This is\nscoreScreen.\nResults will be\nshown here!", skin);
 		
+		//main menu button
+		TextButton mainMenuButton = new TextButton("Main Menu", skin);
+		mainMenuButton.addListener(
+			(Event event) -> {
+				if(!(event instanceof InputEvent)) return false;
+				InputEvent evt = (InputEvent) event;
+				if(evt.getType() != InputEvent.Type.touchDown) return false;
+				notifyScreenChange(new MainMenuScreen(engine, batch));
+				return true;
+				
+			}
+		);
 		// table layout
 		table.add(testLabel).fillX();
 		table.row();
+		table.add(mainMenuButton);
 	}
 	
 	@Override
