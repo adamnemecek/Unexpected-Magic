f095f29b98ddea7e9f9f4b56f5c0676d9efead51
car0b1nius <car0b1nius@localhost>
2017-05-19 12:44:06 +0200

    probably fix the music player

--- core/src/com/mygdx/game/gameEngine/managers/IMusicPlayer.java
+++ core/src/com/mygdx/game/gameEngine/managers/IMusicPlayer.java
@@ -3,7 +3,14 @@ package com.mygdx.game.gameEngine.managers;
 import java.util.Set;
 
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.utils.Disposable;
 
-public interface IMusicPlayer {
-	
+public interface IMusicPlayer extends Disposable {
+	//void add(String name, Music music);
+	void play(String name);
+	void play();
+	void pause();
+	void stop();
+	String playing();
+	Set<String> songs();
 }
--- core/src/com/mygdx/game/gameEngine/managers/MusicPlayer.java
+++ core/src/com/mygdx/game/gameEngine/managers/MusicPlayer.java
@@ -1,30 +1,69 @@
 package com.mygdx.game.gameEngine.managers;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 
-public class MusicPlayer implements IMusicPlayer{
-	private MusicPlayer() {}
-	
-	private final Music mainMenu = Gdx.audio.newMusic(Gdx.files.internal("music/Unexpected_Magic_Theme_by_Isaskar.mp3"));
-
-	@Override
-	public void play(Music music) {
+public class MusicPlayer implements IMusicPlayer {
+	//Singleton implementation
+	private static final MusicPlayer INSTANCE = new MusicPlayer();
+	private MusicPlayer() {
+		Music mainTheme = Gdx.audio.newMusic(Gdx.files.internal("music/Unexpected_Magic_Theme_by_Isaskar.mp3"));
+		mainTheme.setLooping(true);
+		mainTheme.setVolume(0.5f);
+		add("main-theme", mainTheme);
+	}
+	public static MusicPlayer getInstance() {
+		return INSTANCE;
+	}
+	//private final Music mainMenu = Gdx.audio.newMusic(Gdx.files.internal("music/Unexpected_Magic_Theme_by_Isaskar.mp3"));
+	private final Map<String, Music> songs = new HashMap<>();
+	private Music song = null;
+	private String songName = null;
+	private void add(String name, Music music) {
 		music.setLooping(true);
 		music.setVolume(0.5f);
-		music.
+		songs.put(name, music);
 	}
-
+	private void setSong(String name) {
+		if(song != null) song.stop();
+		Music s = songs.get(name);
+		if(s == null) throw new IllegalArgumentException("Unknown song: " + name);
+		songName = name;
+		song = s;
+	}
+	@Override
+	public void play(String name) {
+		setSong(name);
+		play();
+	}
+	@Override
+	public void play() {
+		song.play();
+	}
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
-		
+		song.pause();
 	}
-
 	@Override
 	public void stop() {
-		// TODO Auto-generated method stub
-		
+		song.stop();
 	}
-	
+	@Override
+	public String playing() {
+		return songName;
+	}
+	@Override
+	public Set<String> songs() {
+		return songs.keySet();
+	}
+	@Override
+	public void dispose() {
+		for(Map.Entry<String, Music> e : songs.entrySet()) {
+			e.getValue().dispose();
+		}
+	}
 }
