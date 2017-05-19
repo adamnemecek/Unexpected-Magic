35eed354260580f51b5a7544df903f4e0349cbc3
car0b1nius <car0b1nius@localhost>
2017-05-19 12:05:35 +0200

    test absolutely everything about notes

--- core/test/NoteTest.java
+++ core/test/NoteTest.java
@@ -2,7 +2,6 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import com.mygdx.game.model.song.Note;
@@ -14,7 +13,6 @@ import com.mygdx.game.model.song.Note;
 public class NoteTest {
 	@Test
 	public void testPitch() throws IOException {
-		final int[] scale = new int[] {0,2,4,5,7,9,11};
 		final String[] notes = new String[]{"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
 		for(int i = 0; i <= 127; i++) {
 			int oct = i / 12;
@@ -27,12 +25,40 @@ public class NoteTest {
 	@Test
 	public void testDuration() throws IOException {
 		final String note = "-:%d/%d";
-		for(int denom = 1; denom <= 64; denom *= 2) {
-			for(int num = 1; num < 127; num++) {
+		for(int num = 1; num < 127; num++) {
+			for(int denom = 1; denom <= 64; denom *= 2) {
 				String ns = String.format(note, num, denom);
 				Note n = Note.getNote(ns);
 				assertEquals(ns, num*64/denom, n.getDuration());
 			}
+		}
+	}
+	@Test
+	public void testEverything() throws IOException {
+		for(int num = 1; num < 127; num++) {
+			testAllPitches(Integer.toString(num), num*64);
+			for(int denom = 1; denom <= 64; denom *= 2) {
+				testAllPitches(num + "/" + denom, num*64/denom);
+			}
+		}
+	}
+	final String[] notes = new String[]{"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+	private void testAllPitches(String duration, int expected) throws IOException {
+		{
+			String ns = "-:" + duration;
+			Note n = Note.getNote(ns);
+			assertTrue(ns + " rest", n.isRest());
+			assertEquals(ns + " duration", expected, n.getDuration());
+			String s = ".";
+		}
+		for(int i = 0; i <= 127; i++) {
+			int oct = i / 12;
+			String pitch = notes[i % 12];
+			String ns = pitch + oct + ":" + duration;
+			Note n = Note.getNote(ns);
+			assertEquals(ns + " pitch", i, n.getPitch());
+			assertFalse(ns + " rest", n.isRest());
+			assertEquals(ns + " duration", expected, n.getDuration());
 		}
 	}
 }
