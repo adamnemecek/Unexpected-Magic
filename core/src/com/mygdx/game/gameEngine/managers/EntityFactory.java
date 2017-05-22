b3bd0c761c96cb15ba33bbdbb05559a30edc0846
Rasmus <rtmupp@gmail.com>
2017-05-22 16:43:50 +0200

    create noteentity according to tick

--- core/src/com/mygdx/game/gameEngine/managers/EntityFactory.java
+++ core/src/com/mygdx/game/gameEngine/managers/EntityFactory.java
@@ -4,9 +4,14 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.mygdx.game.gameEngine.components.*;
+import com.mygdx.game.model.Player;
 import com.mygdx.game.model.song.INote;
+import com.mygdx.game.model.song.ISong;
 import com.mygdx.game.model.song.IVoice;
 import com.mygdx.game.utilities.file.Constants;
+
+import java.util.List;
+
 /**
  * A factory class that handles creation of entities.
  * @author soflarb
@@ -19,87 +24,36 @@ public class EntityFactory {
 	private static int nextColor =0;
 	private static final SpriteFactory spriteFactory = new SpriteFactory();
 
-	
-	public static Entity createNoteEntity(INote note, IVoice voice, int playerNumber){
+	public static List<Entity> createNoteEntity(List<Player> players){
+		for (int playerIndex = 0; playerIndex < players.size(); playerIndex ++){
+			Player player = players.get(playerIndex);
+
+			IVoice voice = player.getVoice();
+			for (int tick = 0; tick < voice.length(); tick ++){
+				INote note = voice.noteAtTick(tick);
+				if (note == null || note.isRest()) continue;
+
+				createNoteEntity(note,voice,playerIndex, tick);
+
+			}
+		}
+		return null;
+	}
+
+	public static Entity createNoteEntity(INote note, IVoice voice, int playerNumber, int tick){
 
 		Entity entity = new Entity();
-		float posX = (note.getPitch() % Constants.NUMBER_OF_LANES)*Constants.LANE_WIDTH + (Constants.LANE_WIDTH/2) - 6; //TODO should be spritewidth/2
-		PositionComponent positionComponent = new PositionComponent(posX, NoteOrigPosY);
-		VelocityComponent velocityComponent = new VelocityComponent(0, NoteVelY);
+		int posX = (note.getPitch() % Constants.NUMBER_OF_LANES);
+		PositionComponent positionComponent = new PositionComponent(posX, tick);
 		NoteComponent noteComponent = new NoteComponent(note);
 		CompositeSpriteComponent spriteComponent = new CompositeSpriteComponent(spriteFactory.createSprites(note.getDuration(),playerNumber));
 		HitComponent hitComponent = new HitComponent();
 		VoiceComponent voiceComponent = new VoiceComponent(voice);
-		entity.add(positionComponent).add(velocityComponent).add(noteComponent).add(hitComponent).add(voiceComponent);
+		entity.add(positionComponent).add(noteComponent).add(hitComponent).add(voiceComponent);
 		entity.add(spriteComponent);
 		return entity;
 	}
-	//TODO remove?
-	public static Entity createPlayerEntity(float posX, Sprite sprite){
-		Entity entity = new Entity();
-		/*
-		PositionComponent positionComponent = new PositionComponent(posX, NoteOrigPosY);
-		VelocityComponent velocityComponent = new VelocityComponent(0, NoteVelY);
-		NoteComponent noteComponent = new NoteComponent(note);
-		SpriteComponent spriteComponent = new SpriteComponent(sprite);
-		entity.add(positionComponent).add(velocityComponent).add(noteComponent).add(spriteComponent);
-		*/
-		return entity;
-	}
-	
-	private static Texture newNote(int voiceNum){
-		
-		
-		switch(voiceNum){
-			case 0: return new Texture("sprites/note1pink.png");
-			
-			case 1: return new Texture("sprites/note1blue.png");
-			
-			case 2: return new Texture("sprites/note1yellow.png");
-			
-			case 3: return new Texture("sprites/note1green.png");
-			
-			default: return new Texture("sprites/note1green.png");
-			}
 
-	}
 
 }
-/*
-//Create a test entity----
-Entity testEntity = new Entity();
-PositionComponent testPositionComponent = new PositionComponent();
-VelocityComponent testVelocityComponent = new VelocityComponent();
-SpriteComponent testSpriteComponent = new SpriteComponent();
-RenderableComponent testRenderableComponent = new RenderableComponent();
-//TODO How does one give the components values? I'll do it like this now.
-testPositionComponent.x = 0;
-testPositionComponent.y = Constants.VIEWPORT_DIM_Y;
-testVelocityComponent.x = 0;
-testVelocityComponent.y = -4;
-testSpriteComponent.sprite = new Sprite(new Texture("sprites/note1.png"));
-testEntity.add(testPositionComponent).add(testVelocityComponent).add(testSpriteComponent).add(testRenderableComponent);
-//----
 
-//TODO How does one give the components values? I'll do it like this now.
-testPositionComponent2.x = 20;
-testPositionComponent2.y = Constants.VIEWPORT_DIM_Y;
-testVelocityComponent2.x = 0;
-testVelocityComponent2.y = -4;
-testSpriteComponent2.sprite = new Sprite(new Texture("sprites/note1.png"));
-testEntity2.add(testPositionComponent2).add(testVelocityComponent2).add(testSpriteComponent2).add(testRenderableComponent2);
-//----
-//Add all entities to engine
-//engine.addEntity(testEntity);
-//engine.addEntity(testEntity2);
-
-public Entity createTestEntity(){
-//Create a test entity
-Entity entity = new Entity();
-PositionComponent positionComponent = new PositionComponent();
-VelocityComponent velocityComponent = new VelocityComponent();
-SpriteComponent spriteComponent = new SpriteComponent();
-RenderableComponent renderableComponent = new RenderableComponent();
-return entity;
-}
-*/
