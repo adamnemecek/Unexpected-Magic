9af4edcdcea8afa398b6ac3092157699dfc5d750
Arvid Runvik <ar.netacc@gmail.com>
2017-05-23 11:20:06 +0200

    added trackable note class

---
+++ core/src/com/mygdx/game/model/TrackableNote.java
@@ -0,0 +1,43 @@
+package com.mygdx.game.model;
+
+import com.mygdx.game.model.song.INote;
+
+/**
+ * Class for non-static parts of Notes
+ * @author Arvid
+ *
+ */
+
+public class TrackableNote implements ITrackableNote {
+	
+	private INote note;
+	private boolean isHit;
+	
+	public TrackableNote(INote note){
+		this.note = note;
+		this.isHit = false;
+	}
+
+	@Override
+	public int getPitch() {
+		
+		return note.getPitch();
+	}
+
+	@Override
+	public int getDuration() {
+		return note.getDuration();
+	}
+
+	@Override
+	public boolean isHit() {
+		return isHit;
+	}
+
+	@Override
+	public void hit() {
+		isHit = true;
+
+	}
+
+}
