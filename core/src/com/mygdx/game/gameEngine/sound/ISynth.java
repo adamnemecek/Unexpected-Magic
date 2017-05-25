105286efa1300e53530d2a856d199c8c758de90f
Arvid Runvik <ar.netacc@gmail.com>
2017-05-25 16:25:25 +0200

    Added functions to ISynth

--- core/src/com/mygdx/game/gameEngine/sound/ISynth.java
+++ core/src/com/mygdx/game/gameEngine/sound/ISynth.java
@@ -1,5 +1,20 @@
 package com.mygdx.game.gameEngine.sound;
 
+import com.mygdx.game.model.song.INote;
+
 public interface ISynth {
+	public void setSongTimeSignaure(int timeSignature, int bpm);
+	
+	public void play(INote note);
+	
+	public void play(int noteNumber, int noteDuration);
+	
+	public void noteOn(int noteNumber);
+	
+	public void noteOff(int noteNumber);
+	
+	public void setInstrument(int instrument);
+	
+	public void changeChannel(int channel);
 
 }
