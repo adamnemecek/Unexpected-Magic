0c60ec37dc8e14e291d9fb0a07a30c0c015225e5
Arvid Runvik <ar.netacc@gmail.com>
2017-05-25 18:12:15 +0200

    made HitManager use IPlayer instead of Player

--- core/src/com/mygdx/game/gameEngine/managers/HitManager.java
+++ core/src/com/mygdx/game/gameEngine/managers/HitManager.java
@@ -11,9 +11,10 @@ import java.util.Map;
 import com.mygdx.game.Observers.ObserverHandler;
 import com.mygdx.game.Observers.TickListener;
 import com.mygdx.game.gameEngine.sound.ISynth;
-import com.mygdx.game.gameEngine.sound.Synth;
+
+import com.mygdx.game.model.IPlayer;
 import com.mygdx.game.model.ITrackableNote;
-import com.mygdx.game.model.Player;
+
 import com.mygdx.game.model.TrackableNote;
 
 import com.mygdx.game.model.song.IVoice;
@@ -27,14 +28,14 @@ import com.mygdx.game.utilities.file.Constants;
  */
 
 public class HitManager implements TickListener{
-	private List<Player> players;
+	private List<? extends IPlayer> players;
 	private ISynth synth;
-	private Map<Player, ITrackableNote> activeNotes;
-	private Map<Player, List<Integer>> activePitches;
+	private Map<IPlayer, ITrackableNote> activeNotes; //keeps track of the notes each player is supposed to play
+	private Map<IPlayer, List<Integer>> activePitches; //keeps track of the notes each player is currently playing
 	private int tick;
 	
 	
-	public HitManager(List<Player> players, ISynth synth){
+	public HitManager(List<? extends IPlayer> players, ISynth synth){
 		this.players = players;
 		
 		this.synth = synth;
@@ -42,27 +43,31 @@ public class HitManager implements TickListener{
 		ObserverHandler.addTickListener(this);
 		activeNotes = new HashMap<>();
 		activePitches = new HashMap<>();
-		for (Player p : players){
+		for (IPlayer p : players){
 			activePitches.put(p, new ArrayList<Integer>());
 		}
 	}
 	
-	
+	//Called by InputAction (via RoundManager), interprets which note the key press should be, plays it and logs it
 	public synchronized void notePlayStart(int lane, int player){
 		if(player >= players.size()) return; //TODO remove when multiplayer is properly implemented
-		Player p = players.get(player);
+		IPlayer p = players.get(player);
+
 		if (activeNotes.get(p) != null){
 			ITrackableNote note = activeNotes.get(p);
+			//check if correct
 			if (note.getOctave() == lane){
 				synth.noteOn(note.getPitch());
 				activePitches.get(p).add(note.getPitch());
 			}
+			//if not correct
 			else{
 				int pitch =lane + 12 * p.getVoice().octaveAtTick(tick);
 				synth.noteOn(pitch);
 				activePitches.get(p).add(pitch);
 			}
 		}
+		//if not correct
 		else{
 			int pitch =lane + 12 * p.getVoice().octaveAtTick(tick);
 			synth.noteOn(pitch);
@@ -72,15 +77,16 @@ public class HitManager implements TickListener{
 		
 
 	}
-	
+	//Called by InputAction (via RoundManager), stops playing the note corresponding to the uplifted key and removes it from activePitches
 	public synchronized void notePlayStop(int lane, int player){
 		if(player >= players.size()) return; //TODO remove when multiplayer is properly implemented
-		Player p = players.get(player);
+		
+		IPlayer p = players.get(player);
 		List<Integer> pitches = new ArrayList<>();
-		for(Integer pitch : activePitches.get(p)){
+		for(Integer pitch : activePitches.get(p)){ //creates a copy of ActivePitches
 			pitches.add(pitch);
 		}
-		for (Integer pitch : pitches){
+		for (Integer pitch : pitches){ //goes through the copy, modifies the original with new information
 			if (pitch % Constants.NUMBER_OF_LANES == lane){
 				synth.noteOff(pitch);
 				activePitches.get(p).remove(pitch);
@@ -92,10 +98,11 @@ public class HitManager implements TickListener{
 	}
 
 	@Override
+	//called everytime the game updates its tick, updates score and checks for changes to which notes should be played
 	public void updateTick(int tick) {
 		this.tick = tick;
 		
-		for (Player p : players){
+		for (IPlayer p : players){
 			updateScore(p);
 			IVoice voice = p.getVoice();
 			if (voice.noteAtTick(tick) != null){
@@ -104,7 +111,7 @@ public class HitManager implements TickListener{
 		}	
 	}
 	
-	private void updateScore(Player p){
+	private void updateScore(IPlayer p){
 		ITrackableNote note = activeNotes.get(p); //gets the note the player is supposed to be playing
 		if (note != null){
 			for (Integer pitch : activePitches.get(p)){ 
@@ -121,16 +128,16 @@ public class HitManager implements TickListener{
 		}
 	}
 	
-	private void handleNoteChange(Player p, ITrackableNote note){
-		if (activeNotes.get(p) != null){
+	private void handleNoteChange(IPlayer p, ITrackableNote note){
+		if (activeNotes.get(p) != null){ //checks if there was a note the player was supposed to play before this one
 			ITrackableNote old = activeNotes.get(p);
-			if (!old.isHit()){
+			if (!old.isHit()){ //checks if the player missed the previous note
 				p.getScore().missedNote();
 			}
 			
 			
 		}
-		
+		//updates activeNotes with the new note
 		if (note.isRest()){
 			activeNotes.put(p, null);
 		}
