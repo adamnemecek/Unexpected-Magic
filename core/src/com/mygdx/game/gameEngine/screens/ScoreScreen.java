a51f334ed6d69b9269480c21827bbe52cd75b3f3
sofia.larborn@gmail.com <soflarb@student.chalmers.se>
2017-05-26 13:16:06 +0200

    changed clear color from primary to black in scorescreen

--- core/src/com/mygdx/game/gameEngine/screens/ScoreScreen.java
+++ core/src/com/mygdx/game/gameEngine/screens/ScoreScreen.java
@@ -2,6 +2,8 @@ package com.mygdx.game.gameEngine.screens;
 
 import java.util.List;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Event;
@@ -94,6 +96,8 @@ public class ScoreScreen extends AbstractScreen{
 	@Override
 	public void render(float delta) {
 		super.render(delta);
+		Gdx.gl.glClearColor(0,0,0,1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		camera.update();
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
