6945820ca26770f90934364dbb4145d5fde96623
Car0b1nius <Car0b1nius@users.noreply.github.com>
2017-08-11 19:05:25 +0200

    remove an unnecessary private field

--- core/src/com/mygdx/game/gameEngine/managers/SpriteFactory.java
+++ core/src/com/mygdx/game/gameEngine/managers/SpriteFactory.java
@@ -17,50 +17,39 @@ public class SpriteFactory {
 	public static final int noteSectionHeight = new TextureRegion(new Texture("sprites/note-64th12x4.png")).getRegionHeight();
 	public static final int noteSectionWidth = new TextureRegion(new Texture("sprites/note-64th12x4.png")).getRegionWidth();
 
-	private String color = "";
-	
 	public CompositeSprite createSprites(int duration, int playerNumber, int posX, int posY){
-
+		String color;
 		switch (playerNumber){
-			case(0) : this.color = "-green";
+			case 0: color = "-green";
 				break;
-
-			case(1) : this.color = "-magenta";
+			case 1: color = "-magenta";
 				break;
-
-			case(2) : this.color = "-turquoise";
+			case 2: color = "-turquoise";
 				break;
-
-			case(3) : this.color = "-yellow";
+			case 3: color = "-yellow";
 				break;
-
+			default: throw new IllegalArgumentException("Invalid player index: " + playerNumber);
 		}
 
 		CompositeSprite sprites = new CompositeSprite();
-
 			if(duration == 1) {
-				Sprite sprite = new Sprite(new Texture("sprites/note-64th12x4"+ this.color + ".png"));
+				Sprite sprite = new Sprite(new Texture("sprites/note-64th12x4"+ color + ".png"));
 				sprite.setPosition(posX,posY);
 				sprites.addSprite(sprite);
-			}
-
-			else{
-				Sprite sprite = new Sprite(new Texture("sprites/note-top12x4"+ this.color + ".png"));
+			} else {
+				Sprite sprite = new Sprite(new Texture("sprites/note-top12x4"+ color + ".png"));
 				sprite.setPosition(posX,posY + sprite.getRegionHeight()*(duration-1));
 				sprites.addSprite(sprite);
 
 				for (int i = 0; i < duration-2; i++){
-					sprite = new Sprite(new Texture("sprites/note-mid12x4"+ this.color + ".png"));
+					sprite = new Sprite(new Texture("sprites/note-mid12x4"+ color + ".png"));
 					sprite.setPosition(posX,posY + sprite.getRegionHeight()*(i+1));
 					sprites.addSprite(sprite);
 				}
-				sprite = new Sprite(new Texture("sprites/note-bot12x4"+ this.color + ".png"));
+				sprite = new Sprite(new Texture("sprites/note-bot12x4"+ color + ".png"));
 				sprite.setPosition(posX,posY);
 				sprites.addSprite(sprite);
-
-
 		}
 		return sprites;
 	}
-
 }
